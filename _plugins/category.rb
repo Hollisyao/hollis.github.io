63f4f2d962a6f7a1acc4d36956b5e888242edc58
hollisyao <hollisyao@outlook.com>
2017-01-27 20:07:39 +0800

    修正分类目录的bug

--- _plugins/category.rb
+++ _plugins/category.rb
@@ -1,6 +1,6 @@
 module Jekyll
 
-  class CategoryPage < Page
+  class CategoryIndex < Page
     def initialize(site, base, dir, category)
       @site = site
       @base = base
@@ -11,21 +11,28 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), 'category_index.html')
       self.data['category'] = category
 
-      category_title_prefix = site.config['category_title_prefix'] || ''
+      category_title_prefix = site.config['category_title_prefix'] || 'Category: '
       self.data['title'] = "#{category_title_prefix}#{category}"
     end
   end
 
-  class CategoryPageGenerator < Generator
+  class CategoryGenerator < Generator
     safe true
-
+    
     def generate(site)
       if site.layouts.key? 'category_index'
         dir = site.config['category_dir'] || 'categories'
         site.categories.keys.each do |category|
-          site.pages << CategoryPage.new(site, site.source, File.join(dir, category), category)
+          write_category_index(site, File.join(dir, category), category)
         end
       end
+    end
+  
+    def write_category_index(site, dir, category)
+      index = CategoryIndex.new(site, site.source, dir, category)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
     end
   end
 
